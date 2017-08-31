db19913a09fdc68fef4663c55c300e0edf21fe8c
Nut <nuttapon.tavee@gmail.com>
2017-08-31 16:01:22 +0700

    Initial commit

---
+++ client/src/containers/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Provider} from 'react-redux'
+import {Route, Switch} from 'react-router'
+import {CreateArticle, EditArticle, ShowArticle, Articles} from 'Containers'
+import {loadState, configureStore} from 'Lib'
+import DevTools from 'Containers/DevTools'
+
+const store = configureStore(loadState());
+
+export default () => (
+  <Provider store={store}>
+    <div>
+      <Switch>
+        <Route path='/articles/new' component={CreateArticle}/>
+        <Route path='/articles/:id/edit' component={EditArticle}/>
+        <Route path='/articles/:id' component={ShowArticle}/>
+        <Route path='/articles' component={Articles}/>
+      </Switch>
+      <DevTools/>
+    </div>
+  </Provider>
+)
---
+++ client/src/containers/Articles.js
@@ -0,0 +1,29 @@
+import React, {PureComponent} from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+class Articles extends PureComponent {
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            this.props.articles.map(
+              article =>
+                <li key={article.id}>
+                  <Link to={`/articles/${article.id}`}>{article.value}</Link>
+                </li>
+            )
+          }
+        </ul>
+        <Link to='/articles/new'>New Article</Link>
+      </div>
+    )
+  }
+}
+
+export default connect(
+  ({articles}) => ({
+    articles
+  })
+)(Articles)
